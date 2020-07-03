a237b09937c13c775ed945cc4b6bd152196bd03c
Cody Alvarez <cody_alvarez19@outlook.com>
2020-07-03 20:23:24 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -2,6 +2,8 @@ class CreateStudents < ActiveRecord::Migration[5.2]
   def change
     create_table :students do |t|
       t.string :name
+      t.integer :grade
+      t.string :birthdate
     end
   end
 
