95d7ae647ea92b2d0184f6707574a4058fa63169
Cody Alvarez <cody_alvarez19@outlook.com>
2020-07-03 20:46:52 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,6 @@
 class ChangeDataTypeForBirthdate < ActiveRecord::Migration[5.2]
   def change
-    change_column (:students, :birthdate, :datetime)
+    change_column :students, :birthdate, :datetime
     end
   end
 end
